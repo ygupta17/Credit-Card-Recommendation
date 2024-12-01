519981374eae067fbaa807bd96f557b2063c35d2
Yash Gupta <yash@Yashs-MacBook-Air-3.local>
2024-12-01 12:11:37 -0500

    XCode setup

---
+++ CardFit/CardFit/CardFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFitApp.swift
+//  CardFit
+//
+//  Created by Yash Gupta on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CardFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardFit/CardFit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CardFit
+//
+//  Created by Yash Gupta on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Testing")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
